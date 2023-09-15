90b65df9493b1654ce50552a38f338b0313b3c61
Aubrey Dugger <adugger@ucar.edu>
2023-09-15 09:05:36 -0400

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -84,8 +84,8 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-
 html_logo = "climate_primer_small.png"
+html_show_sourcelink = True
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
